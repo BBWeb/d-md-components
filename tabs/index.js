3c3744da97a4663b4a7b1c35121464e395b545e7
Johannes Lilja <joh.lilja@gmail.com>
2015-11-19 11:14:50 +0100

    Refactor component to work with dynamic height content

--- tabs/index.js
+++ tabs/index.js
@@ -10,9 +10,16 @@ Tabs.prototype.components = [];
 require('./operations');
 require('./actions');
 
+Tabs.prototype.init = function(model) {
+  var tabs = this.getAttribute('tab')
+
+  if (tabs) this._assignPositions(tabs);
+}
+
+
 Tabs.prototype.create = function(model) {
   var index = this.getAttribute('selectedTab') || 0;
   var el = document.getElementById('tab-header-' + index);
 
-  this._selectTab(index, el);
+  if (el) this._selectTab(index, el);
 };
--- tabs/operations/index.js
+++ tabs/operations/index.js
@@ -1,7 +1,71 @@
 var Tabs = require('./../index');
+var _ = require('lodash');
 
 Tabs.prototype._selectTab = function (index, el) {
-  this.model.setEach('selectedTab',  { index : index, height: el.clientHeight + el.nextElementSibling.clientHeight });
+  // Only animate if another tab is leaving view.
+  var leavingIndex = this.model.get('selectedTab');
+  if (leavingIndex !== undefined) this._animateTab(index, leavingIndex);
 
+  this.model.set('selectedTab', index);
   this.model.setEach('tabsUnderline', { left: el.offsetLeft, width: el.clientWidth });
-}
+};
+
+Tabs.prototype._animateTab = function (index, leavingIndex) {
+  var selectedTab = document.getElementById('tab-content-' + index);
+  var leavingTab = document.getElementById('tab-content-' + leavingIndex);
+
+  var reset = selectedTab.className;
+  var leavingMax = leavingTab.scrollHeight + 'px';
+  
+  selectedTab.className += ' no-transition';
+  leavingTab.style.maxHeight = leavingMax;
+  selectedTab.style.maxHeight = leavingMax;
+  // Force style recalculation for transitions to work. See http://stackoverflow.com/questions/18564942/clean-way-to-programmatically-use-css-transitions-from-js/31862081
+  window.getComputedStyle(selectedTab).maxHeight
+  selectedTab.className = reset;
+
+  this._setAnimation();  
+  this.model.set('viewHeight', selectedTab.scrollHeight);
+};
+
+Tabs.prototype._assignPositions = function (tabs) {
+ var self = this;
+ var offset = 100/tabs.length;
+
+  _.each(tabs, function(tab, index) {
+    var before = -(index * offset);
+    var selected;
+    var after;
+
+    if (index === 0) { 
+      selected = offset;
+    } else if (index === 1) {
+      selected = 0;
+    } else {
+      selected = -((index * offset) - offset);
+    }
+
+    if (index === 1) { 
+      after = offset;
+    } else if (index === 2) {
+      after = 0;
+    } else {
+      after = -((index * offset) - (offset * 2));
+    }
+
+    self.model.setEach('tabPositions.' + index, {
+      selected: selected,
+      before: before,
+      after: after
+    });
+  });
+};
+
+Tabs.prototype._setAnimation = function() {
+  var self = this;
+
+  this.model.set('animating', true);
+  setTimeout(function(){
+    self.model.set('animating', false);
+  }, 300);
+};
