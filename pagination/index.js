ac73dfb3b6fddbaaae12e1202d4d88f6cc393d7f
cjblomqvist <carl@cjblomqvist.com>
2015-12-14 12:05:13 +0100

    Don't put helper fns anywhere else than where they belong, such as if it were a view helper

--- pagination/index.js
+++ pagination/index.js
@@ -16,7 +16,7 @@ require('./viewhelpers');
 Pagination.prototype.init = function(model) {
   if(typeof model.get('end') !== 'undefined') this.initPages();
 
-  model.start('visibleNumbers', 'activePage', 'pages', this.getVisibleNumbers);
+  model.start('visibleNumbers', 'activePage', 'pages', getVisibleNumbers);
 };
 
 Pagination.prototype.initPages = function () {
@@ -26,4 +26,27 @@ Pagination.prototype.initPages = function () {
 
   this.model.start('pages', 'end', 'pages');
   this.model.get('pages');
+};
+
+// Helper fn for getting the currently visible page numbers
+function getVisibleNumbers(activePage, pages) {
+  var visibleNumbers = [];
+  var nrOfPages = pages.length;
+  var firstPage = activePage - 4;
+
+  // Get numbers around active page
+  if (firstPage > (nrOfPages - 8)) firstPage = (nrOfPages - 8);
+  if (firstPage < 2) firstPage = 1;
+
+  for (var i = firstPage - 1; i < (firstPage + 8); i++) {
+    if(pages[i]) visibleNumbers.push(pages[i]);
+  };
+
+  // Remove numbers if ellipsis is showing so the paginator stays the same size.
+  if(visibleNumbers[0] > 1) visibleNumbers = visibleNumbers.slice(2);
+  if(visibleNumbers[visibleNumbers.length - 1] < pages.length) {
+    visibleNumbers = visibleNumbers.slice(0, -2);
+  }
+
+  return visibleNumbers;
 };
