0284291f9b21e0091db32bea112b0fd231ea2742
Johannes Lilja <joh.lilja@gmail.com>
2016-01-12 09:21:47 +0100

    Add missing action

--- select/actions/index.js
+++ select/actions/index.js
@@ -3,3 +3,4 @@ Select.prototype.willSelect = Select.prototype._willSelect;
 Select.prototype.adjustDropdown = Select.prototype._adjustDropdown;
 Select.prototype.addCloseListener = Select.prototype._addCloseListener;
 Select.prototype.removeCloseListener = Select.prototype._removeCloseListener;
+Select.prototype.select = Select.prototype._select;
