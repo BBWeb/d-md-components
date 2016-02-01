2d021a32d477a18bbb81261eca048fadad22dd9e
Johannes Lilja <joh.lilja@gmail.com>
2016-02-01 11:19:24 +0100

    Add month/year pickers and styling.

--- datepicker/actions/index.js
+++ datepicker/actions/index.js
@@ -1,3 +1,9 @@
 var Datepicker = require('./../index');
 
+Datepicker.prototype.selectDate = Datepicker.prototype._selectDate;
+Datepicker.prototype.selectYear = Datepicker.prototype._selectYear;
+Datepicker.prototype.selectMonth = Datepicker.prototype._selectMonth;
+
+Datepicker.prototype.showView = Datepicker.prototype._showView;
+
 Datepicker.prototype.flipMonth = Datepicker.prototype._flipMonth;
--- datepicker/operations/index.js
+++ datepicker/operations/index.js
@@ -1,8 +1,42 @@
 var Datepicker = require('./../index');
 
+Datepicker.prototype._showView = function(view) {
+  if (this.model.get('activeView') === view) return;
+
+  this.model.set('activeView', view);
+
+  if (view === 'day') this._setMonthMinHeight(this.model.get('isMonthViewOne') ? 0 : 1);
+  if (view === 'year') this._scrollToSelected(this.$yearlist);
+  if (view === 'month') this._scrollToSelected(this.$monthlist);
+};
+
+Datepicker.prototype._selectDate = function(day) {
+  this.model.set('selectedDate', day.date);
+};
+
+Datepicker.prototype._selectYear = function(year) {
+  this.model.set('selectedYear', year);
+
+  // REVIEW: center on selection or only on view load?
+  this._scrollToSelected(this.$yearlist);
+};
+
+Datepicker.prototype._selectMonth = function(month) {
+  this.model.set('selectedMonth', month);
+  
+  // REVIEW: center on selection or only on view load?
+  this._scrollToSelected(this.$monthlist);
+};
+
+Datepicker.prototype._scrollToSelected = function($list) {
+  var $item = $list.querySelector('.pickerlist---item--selected');
+
+  $list.scrollTop = ($item.offsetTop) - (($list.clientHeight / 2) - ($item.clientHeight / 2));
+};
+
 Datepicker.prototype._flipMonth = function(direction) {
   // Figure out which view is entering/leaving.
-  var isFirstActive = this.model.set('firstViewActive', !this.model.get('firstViewActive'));
+  var isFirstActive = this.model.set('isMonthViewOne', !this.model.get('isMonthViewOne'));
   var entering = (isFirstActive) ? 'monthTwo' : 'monthOne';
   var leaving = (!isFirstActive) ? 'monthTwo' : 'monthOne';
   var to = (direction === 'right') ? 'left' : 'right';
@@ -17,17 +51,17 @@ Datepicker.prototype._flipMonth = function(direction) {
 
   // Set maxHeight on month container so it animates. This is a good place to do this since setting min-height
   // will also force a redrawing - giving time to position entering class without transition.
-  this.setMonthMinHeight(isFirstActive);
+  var enteringChildIndex = (isFirstActive) ? 1 : 0;
+  this._setMonthMinHeight(enteringChildIndex);
 
   // Set entering view to active and leaving views to-direction to animate slide.
   this.model.set(entering + 'PositionClass', 'days---dates--active');
   this.model.set(leaving + 'PositionClass', 'days---dates--' + to);
 };
 
-Datepicker.prototype.setMonthMinHeight = function (isFirstChild) {
-  var enteringChildIndex = (isFirstChild) ? 1 : 0;
+Datepicker.prototype._setMonthMinHeight = function (childIndex) {
   var children = this.$months.childNodes;
-  var enteringHeight = children[enteringChildIndex].offsetHeight;
+  var childHeight = children[childIndex].offsetHeight;
 
-  this.$months.style.minHeight = enteringHeight + 'px';
+  this.$months.style.minHeight = childHeight + 'px';
 };
