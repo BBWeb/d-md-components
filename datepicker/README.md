122c3e87318c5811e4839e548994d82ab0911139
Johannes Lilja <joh.lilja@gmail.com>
2016-02-04 10:35:07 +0100

    Add documentation

--- datepicker/README.md
+++ datepicker/README.md
@@ -4,6 +4,9 @@ A datepicker component.
 Features
 --------
 - A datepicker styled according to material design.
+- Optional extra buttons displayed next to OK/Cancel.
+- Min/max-range.
+
 
 Usage
 -----
@@ -15,6 +18,37 @@ app.component(mdlComponents.Datepicker);
 ```
 
 #### Template
-```html
-  
+```html  
+  <view is="datepicker" 
+    as="$datepicker" 
+    value="{{date}}" 
+    inline="{{false}}" 
+    actions="{{['Tomorrow', { text: 'Yesterday', class: 'blue', disableDismiss: true }]}}" 
+    on-select="foo()"
+    on-action="bar(action)"
+    on-show="baz()"
+    on-hide="qux()" 
+    options="{{
+      {
+        locale: 'sv',
+        accent: 'rgb(0, 188, 212)',
+        okText: 'Righty-O',
+        cancelText: 'Forget it',
+        disableCancel: false,
+        autoOk: false,
+        minDate: 2015-01-01,
+        maxDate: 2020-12-31
+      }
+    }}" 
+  />
 ```
+
+## Public Methods
+
+### this.$datepicker.show()
+
+Displays the datepicker.
+
+### this.$datepicker.hide()
+
+Hides the datepicker programatically.
--- datepicker/index.js
+++ datepicker/index.js
@@ -3,7 +3,28 @@ var moment = require('moment/min/moment-with-locales.min');
 module.exports = Datepicker;
 
 /**
- * Creates a datepicker component.
+ * A datepicker component which can show additional buttons and fire events for them.
+ *
+ * @fires Datepicker#show     - Fired when the Datepicker is shown.
+ * @fires Datepicker#hide     - Fired when the Datepicker is hidden.
+ * @fires Datepicker#selected - Fired when a user clicks Ok action OR when user clicks a date if autoOk === true.
+ * @fires Datepicker#action   - Fired when a user clicks an optinal provided button.
+ *
+ * @param {Model||String}      [value]           - A path to write the selected value to or a date string to use as initial value.
+ * @param {Boolean}            [inline]          - Default false. Decides whether the Datepicker displays in the middle of container (preferably body) or inline.
+ * @param {Object[]||String[]} [actions]         - An array of actions displayed as buttons. Buttons emits the string back when pressed. Can also be an array of objects with options for the buttons.
+ *   @param {String}           text              - Mandatory if actions are objects. Used as button text and passed in the event.
+ *   @param {Boolean}          [disableDismiss]  - Default false. Actions dismiss the Datepicker by default. Set this true if that's not the desired behaviour.
+ *   @param {String}           [class]           - A class to use for this specific button. Buttons will use the optional accent color by default unless this class is provided.
+ * @param {Object}             [options]         - An options object.
+ *   @param {String}           [locale]          - A locale string that works with moment ('en'). Default language is english.
+ *   @param {String}           [accent]          - A css string to style the datepicker.
+ *   @param {String}           [okText]          - A string to replace the select-and-close button text. Default is 'OK'.
+ *   @param {String}           [cancelText]      - A string to replace the dismiss button text. Default is 'Cancel'.
+ *   @param {Boolean}          [disableCancel]   - Default false. If true, removes the Cancel-button. Dismissal of the datepicker will still be possible by clicking outside or Ok/autoclose.
+ *   @param {Boolean}          [autoOk]          - Default false. If true, removes the OK-button and selects date as soon as user clicks a date.
+ *   @param {Object|String}    [minDate]         - Default value||today - 100 years. The beginning of a range of valid dates. The range includes the minDate.
+ *   @param {Object|String}    [maxDate]         - Default value||today + 100 years. The end of a range of valid dates. The range includes the maxDate.
  */
 function Datepicker() {}
 
